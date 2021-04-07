29b2a12a00673e2a6c2f48032d4269db99096ea4
SebastianMxpwr <74206444+SebastianMxpwr@users.noreply.github.com>
2021-04-07 17:51:23 -0500

    Update propiedas.js

--- server/config/propiedas.js
+++ server/config/propiedas.js
@@ -1,4 +1,4 @@
 module.exports={
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb://localhost:27017/proyectoDm'
+    DB: 'mongodb+srv://Admin:admin123@cluster0.i0pvv.mongodb.net/ProyectoDm?retryWrites=true&w=majority'
 }
